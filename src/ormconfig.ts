aef0621502045b6b84b9f0cd1d580aaa558c67b9
Matheus Henrique Almeida Vieira Neves <matheus16092004@gmail.com>
2025-03-21 08:01:18 -0300

    commit inicial do projeto em next

---
+++ src/ormconfig.ts
@@ -0,0 +1,7 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+export const config: TypeOrmModuleOptions = {
+    type:'sqlite',
+    database: 'database.sqlite',
+    entities: [__dirname+'/**/*.entity{.ts,.js}'],
+    synchronize: true,
+}
